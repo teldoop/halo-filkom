6ca4f6c73b45f425a37b2e3fd4c5b1fcace64484
teldoop <admin@teldoop.my.id>
2026-09-12 02:11:05 +0700

    Add Halo FILKOM java file

---
+++ halofilkom.java
@@ -0,0 +1,7 @@
+public class halofilkom {
+    public static void main(String[] args) {\
+        // Halo FILKOM digunakan sebagai pengganti Hello World!
+        // Halo FILKOM is used for Hello World! Alternative.
+        System.out.println("Halo, Filkom!");
+    }
+}
